d52028c620239ff2d26ea46a09d54235dbe27d6c
DiogoCaverna <caverna666@outlook.com>
2024-05-05 13:24:39 -0300

    Inserindo a tag user_data

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   key_name = "aluraIAC"
-
+  user_data = <<-EOF
+    #!/bin/bash
+    cd home/ubuntu
+    echo "<h1>FeitoTerraform</h1>" > index.txt
+    nohup busybox httpd -f -p 8080 &
+ 	      EOF
+  user_data_replace_on_change = true
   tags = {
-    Name = "PrimeiraInstancia"
+    Name = "Terraform1"
   }
 }
