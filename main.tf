8e5a06290838844efc6e2fa327c5a59127b11ab3
DiogoCaverna <caverna666@outlook.com>
2024-05-08 20:42:55 -0300

    playbook e hosts criados

--- main.tf
+++ main.tf
@@ -14,17 +14,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-04b70fa74e45c3917"
+  #ami           = "ami-04b70fa74e45c3917"  
+  ami           = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.micro"
   key_name = "aluraIAC"
-  user_data = <<-EOF
+  # user_data = <<-EOF
     #!/bin/bash
-    cd home/ubuntu
-    echo "<h1>FeitoTerraform</h1>" > index.txt
-    nohup busybox httpd -f -p 8080 &
- 	      EOF
-  user_data_replace_on_change = true
+    # cd home/ubuntu
+    # echo "<h1>FeitoTerraform</h1>" > index.html
+    #nohup busybox httpd -f -p 8080 &
+    #   EOF
+  #user_data_replace_on_change = true
   tags = {
-    Name = "Terraform1"
+    Name = "TerraformAnsiblePython"
   }
 }
